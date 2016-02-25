997a5f642dd0400329a6a305141f9100b3da720d
Sivynia <cfan12@illinois.edu>
2016-02-24 22:30:28 -0600

    fixed bugs

--- scraper.py
+++ scraper.py
@@ -20,7 +20,7 @@ try:
 except IndexError:
     num_post = 1
 	
-url_list = ["{}&cid={}".format(base_url, str(page)) for page in range(1, num_post + 1)
+url_list = ["{}&cid={}".format(base_url, str(page)) for page in range(1, num_post + 1)]
 
 views = []
 question = []
@@ -32,19 +32,17 @@ instructoranswer = []
 instructthanks = []
 discussion = []
 
-
-print "Parsing data..."
-for html in metaData:
-    text = BeautifulSoup(str(html).strip()).get_text().encode("utf-8").replace("\n", "") 
-    views.append(text.split("question")[1].split("views")[0].strip()) 												#get views
-    question.append(text.split("views")[1].split("good question")[0].strip()) 										#get question
-	goodquestion.append(text.split("good question")[1].split("Updated")[0].strip()) 								#get good question
-	folder.append(text.split('\n')[1].split("good question")[0].strip()) 											#get folder
-	studentanswer.append(text.split("collectively construct a single answer")[1].split("thanks!")[0].strip())		#get student answer
-	studthanks.append(text.split("thanks")[1].split("updated")[0].strip()) 											#get student thanks
-	instructoranswer.append(text.split("the instructors' answer,")[1].split("thanks")[0].strip()) 					#get instructor answer
-	instructthanks.append(text.split("thanks")[1].split("updated")[0].strip()) 										#get instruct thanks
-	discussion.append(text.split("questions and comments")[1].split("Start a new follow-up discussion")[0].strip()) #get discussion
+for url_ in url_list:
+ 	text = BeautifulSoup(str(html).strip()).get_text().encode("utf-8").replace("\n", "") 
+	views.append(text.split("question")[1].split("views")[0].strip())#get views
+	question.append(text.split("views")[1].split("good question")[0].strip())#get question
+	goodquestion.append(text.split("good question")[1].split("Updated")[0].strip())#get good question
+	folder.append(text.split('\n')[1].split("good question")[0].strip())#get folder
+	studentanswer.append(text.split("collectively construct a single answer")[1].split("thanks!")[0].strip())#get student answer
+	studthanks.append(text.split("thanks")[1].split("updated")[0].strip())#get student thanks
+	instructoranswer.append(text.split("the instructors' answer,")[1].split("thanks")[0].strip())#get instructor answer
+	instructthanks.append(text.split("thanks")[1].split("updated")[0].strip())#get instruct thanks
+	discussion.append(text.split("questions and comments")[1].split("Start a new follow-up discussion")[0].strip())#get discussion
 
 for post in postData:
     posts.append(BeautifulSoup(str(post)).get_text().encode("utf-8").strip())
@@ -53,6 +51,6 @@ csvfile = open('piazza225fa15.csv', 'wb')
 writer = csv.writer(csvfile)
 writer.writerow(["Views", "Question", "Good Question", "Folder", "Student Answer", "Student Thanks", "Instructor Answer", "Instructor Thanks", "Discussion"])
 
-for views, question, goodquestion, studentanswer, studthanks, instructoranswer, instructthanks, discussion post in zip(views, question, goodquestion, studentanswer, studthanks, instructoranswer, instructthanks, discussion):
+for views, question, goodquestion, studentanswer, studthanks, instructoranswer, instructthanks, discussion in zip(views, question, goodquestion, studentanswer, studthanks, instructoranswer, instructthanks, discussion):
     writer.writerow([views, question, goodquestion, studentanswer, studthanks, instructoranswer, instructthanks, discussion])
 csvfile.close()
